8bd0aae0b8b22423762115e8372522c45a0ad41e
Vovarama1992 <vovayhh9988@gmail.com>
2024-08-22 14:36:08 +0300

    salary

--- src/pages/workers/ui/EditWorkerForm.tsx
+++ src/pages/workers/ui/EditWorkerForm.tsx
@@ -13,12 +13,25 @@ const EditWorkerForm: React.FC<EditWorkerFormProps> = ({ existingWorker, onClose
 
   const [formData, setFormData] = useState<WorkerDto>({
     ...existingWorker,
+    roleName: '', // Default value for role
+    salary: existingWorker.salary || 0, // Default value for salary
   })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target
 
-    setFormData(prevData => ({ ...prevData, [name]: value }))
+    // Convert salary to number if the field is salary
+    if (name === 'salary') {
+      setFormData(prevData => ({
+        ...prevData,
+        [name]: parseFloat(value) || 0, // Convert to float
+      }))
+    } else {
+      setFormData(prevData => ({
+        ...prevData,
+        [name]: value,
+      }))
+    }
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -33,119 +46,177 @@ const EditWorkerForm: React.FC<EditWorkerFormProps> = ({ existingWorker, onClose
 
   return (
     <div className={'fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center'}>
-      <div className={'bg-white p-6 rounded-lg shadow-lg'}>
+      <div className={'bg-white p-6 rounded-lg shadow-lg max-w-md w-full'}>
         <h2 className={'text-lg font-semibold mb-4'}>Редактировать сотрудника</h2>
-        <form onSubmit={handleSubmit}>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Фамилия</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'surname'}
-              onChange={handleChange}
-              required
-              type={'text'}
-              value={formData.surname}
-            />
+        <form className={'grid grid-cols-1 gap-4'} onSubmit={handleSubmit}>
+          <div className={'grid grid-cols-2 gap-4'}>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Фамилия</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'surname'}
+                onChange={handleChange}
+                required
+                type={'text'}
+                value={formData.surname}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Имя</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'name'}
+                onChange={handleChange}
+                required
+                type={'text'}
+                value={formData.name}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Отчество</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'middleName'}
+                onChange={handleChange}
+                type={'text'}
+                value={formData.middleName}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Должность</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'position'}
+                onChange={handleChange}
+                required
+                type={'text'}
+                value={formData.position}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Почта</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'email'}
+                onChange={handleChange}
+                required
+                type={'email'}
+                value={formData.email}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Добавочный номер</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'dobNumber'}
+                onChange={handleChange}
+                type={'text'}
+                value={formData.dobNumber}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Мобильный телефон</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'mobile'}
+                onChange={handleChange}
+                required
+                type={'text'}
+                value={formData.mobile}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Дата рождения</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'birthday'}
+                onChange={handleChange}
+                required
+                type={'date'}
+                value={formData.birthday}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Дата приема на работу</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'hireDate'}
+                onChange={handleChange}
+                required
+                type={'date'}
+                value={formData.hireDate}
+              />
+            </div>
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Карта для перевода</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'cardNumber'}
+                onChange={handleChange}
+                type={'text'}
+                value={formData.cardNumber}
+              />
+            </div>
+
+            {/* New field for salary */}
+            <div className={'flex flex-col'}>
+              <label className={'block text-gray-700'}>Зарплата</label>
+              <input
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'salary'}
+                onChange={handleChange}
+                type={'number'}
+                value={formData.salary}
+              />
+            </div>
+
+            {/* New field for role selection */}
+            <div className={'flex flex-col col-span-2'}>
+              <label className={'block text-gray-700'}>Роль</label>
+              <select
+                className={
+                  'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'
+                }
+                name={'roleName'}
+                onChange={handleChange}
+                value={formData.roleName}
+              >
+                <option value={''}>Выберите роль</option>
+                <option value={'Менеджер'}>Менеджер</option>
+                <option value={'РОП'}>РОП</option>
+                <option value={'Директор'}>Директор</option>
+                <option value={'Логист'}>Логист</option>
+                <option value={'Закупщик'}>Закупщик</option>
+                <option value={'Бухгалтер'}>Бухгалтер</option>
+              </select>
+            </div>
           </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Имя</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'name'}
-              onChange={handleChange}
-              required
-              type={'text'}
-              value={formData.name}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Отчество</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'middleName'}
-              onChange={handleChange}
-              type={'text'}
-              value={formData.middleName}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Должность</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'position'}
-              onChange={handleChange}
-              required
-              type={'text'}
-              value={formData.position}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Почта</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'email'}
-              onChange={handleChange}
-              required
-              type={'email'}
-              value={formData.email}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Добавочный номер</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'dobNumber'}
-              onChange={handleChange}
-              type={'text'}
-              value={formData.dobNumber}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Мобильный телефон</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'mobile'}
-              onChange={handleChange}
-              required
-              type={'text'}
-              value={formData.mobile}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Дата рождения</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'birthday'}
-              onChange={handleChange}
-              required
-              type={'date'}
-              value={formData.birthday}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Дата приема на работу</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'hireDate'}
-              onChange={handleChange}
-              required
-              type={'date'}
-              value={formData.hireDate}
-            />
-          </div>
-          <div className={'mb-4'}>
-            <label className={'block text-gray-700'}>Карта для перевода</label>
-            <input
-              className={'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm'}
-              name={'cardNumber'}
-              onChange={handleChange}
-              type={'text'}
-              value={formData.cardNumber}
-            />
-          </div>
-          <div className={'flex justify-end'}>
+
+          <div className={'flex justify-end gap-2 mt-4'}>
             <button
-              className={'mr-2 px-4 py-2 bg-gray-300 rounded-lg'}
+              className={'px-4 py-2 bg-gray-300 rounded-lg'}
               onClick={onClose}
               type={'button'}
             >
